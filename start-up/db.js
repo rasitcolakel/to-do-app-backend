e046e6eedd4d5e7e9469ea6be084b2a7c212e449
Raşit <rasitcolakel@hotmail.com>
2021-09-04 02:17:11 +0300

    added

--- start-up/db.js
+++ start-up/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb://localhost";
+const uri =
+  "mongodb+srv://admin:j9WU7MQ0gT9H4yBH@cluster0.pijyl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 module.exports = () => {
   mongoose
     .connect(uri, {
